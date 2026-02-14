e14a2005eb6c3a261dc315dfc2f8b1c691a1b209
LaurenceTaylor <morfeus06388casa@gmail.com>
2026-02-13 18:04:54 -0600

    update main.py

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
+# Function to calculate the longest time in the array
 def longest(skater):
-    # Initialize Skater
-    max_skater = 0
+    # Init max_time
+    max_time = 0
+    # Iterate over all elements in the times array
     for i in range(len(skater)):
-        if max_skater < skater[i]:
-            max_skater = skater[i]
-            pos_skater = i + 1
-    return (max_skater, pos_skater)
+        # if is max_time
+        if max_time < skater[i]:
+            # update max_time
+            max_time = skater[i]
+            # store lap number
+            lap_number = i + 1
+    return (max_time, lap_number)
 
 def largest_difference(skater1, skater2):
-    max_skater1, pos_skater1 = longest(skater1)
-    max_skater2, pos_skater2 = longest(skater2)
-    if max_skater1 > max_skater2:
-        return pos_skater1
+    # Find longest lap and lap number of skater1
+    longest_lap_1, lap_no_1 = longest(skater1)
+    # Find longest lap and lap number of skater1
+    longest_lap_2, lap_no_2 = longest(skater2)
+    # Return number of the longest lap
+    if longest_lap_1 > longest_lap_2:
+        return lap_no_1
     else:
-        return pos_skater2
+        return lap_no_2
 
-pos = largest_difference([26.11, 25.80, 25.92, 26.23, 26.07], [25.93, 25.74, 26.53, 26.11, 26.30])
-print(pos)
+print(largest_difference([26.11, 25.80, 25.92, 26.23, 26.07], [25.93, 25.74, 26.53, 26.11, 26.30]))
